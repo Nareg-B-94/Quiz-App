9adbe5d2613475bb37d894f64ee55d23309ffb88
Nareg-B-94 <naregb94@gmail.com>
2022-06-07 11:52:23 +0200

    with blocked feature and second Category but still in process

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let questions = [
         "option2": "Bayrische Klöße",
         "option3": "Thüringer Klöße",
         "option4": "Frankfurter Klöße",
-        "rightAnswer": "3"
+        "rightAnswer": "3",
+        "category": "deutsche Meme & flach Witze",
     },
     {
         "question": "Was ist den mit ......... los?",
@@ -13,7 +14,8 @@ let questions = [
         "option2": "Thomas",
         "option3": "Helmut",
         "option4": "Karsten",
-        "rightAnswer": "4"
+        "rightAnswer": "4",
+        "category": "deutsche Meme & flach Witze",
     },
     {
         "question": "wer ist der wasch echter Isländer?",
@@ -21,7 +23,8 @@ let questions = [
         "option2": "sein Bruder Jürgen Hurensohn",
         "option3": "David Gudjohnsohn",
         "option4": "Björn Bjorensohn",
-        "rightAnswer": "2"
+        "rightAnswer": "2",
+        "category": "deutsche Meme & flach Witze",
     },
     {
         "question": "wer ist von den 4 hier ! nicht ! am Samstag für die fette Party dabei?",
@@ -29,7 +32,8 @@ let questions = [
         "option2": "Patrick",
         "option3": "UWE",
         "option4": "Gundula :D",
-        "rightAnswer": "4"
+        "rightAnswer": "4",
+        "category": "deutsche Meme & flach Witze",
     },
     {
         "question": "was hat Walter Frosch versucht Jahre lang zu machen?",
@@ -37,7 +41,8 @@ let questions = [
         "option2": "versucht die Kippe aus Mund weg zu halten",
         "option3": "in der schnelle eine durchziehen",
         "option4": "eeee keine Ahnung!",
-        "rightAnswer": "1"
+        "rightAnswer": "1",
+        "category": "deutsche Meme & flach Witze",
     },
     {
         "question": "Was sagt ein Gen, wenn es ein anderes trifft?",
@@ -45,7 +50,8 @@ let questions = [
         "option2": "Halogen",
         "option3": "moinsen",
         "option4": "ciao papito",
-        "rightAnswer": "2"
+        "rightAnswer": "2",
+        "category": "deutsche Meme & flach Witze",
     },
     {
         "question": "Egal, wie gut du schläfst: Albert schläft wie...",
@@ -53,7 +59,8 @@ let questions = [
         "option2": "ein Block",
         "option3": "Einstein",
         "option4": "drei Steine vielleicht?",
-        "rightAnswer": "3"
+        "rightAnswer": "3",
+        "category": "deutsche Meme & flach Witze",
     },
     {
         "question": "Was hat vier Beine und kann fliegen?",
@@ -61,7 +68,8 @@ let questions = [
         "option2": "einer der Red Bull rein knallt!",
         "option3": "ja du vielleicht! wenn du besoffen bist :D",
         "option4": "brudi muss los keine Ahnung",
-        "rightAnswer": "1"
+        "rightAnswer": "1",
+        "category": "deutsche Meme & flach Witze",
     },
     {
         "question": "Wie lautet der Vorname vom Reh?",
@@ -69,7 +77,8 @@ let questions = [
         "option2": "Kartoffelpü",
         "option3": "reh-reh aber klein geschrieben",
         "option4": "....",
-        "rightAnswer": "2"
+        "rightAnswer": "2",
+        "category": "deutsche Meme & flach Witze",
     },
     {
         "question": "Welche Schuhe tragen BILD-Redakteure?",
@@ -77,7 +86,98 @@ let questions = [
         "option2": "Flip-Flops",
         "option3": "Yeezys",
         "option4": "Skandalen",
-        "rightAnswer": "4"
+        "rightAnswer": "4",
+        "category": "deutsche Meme & flach Witze",
+    },
+    {
+        "question": "who invented HTML?",
+        "option1": "Lady GaGa",
+        "option2": "Tim Berners-Lee",
+        "option3": "Dr. Frankenstein",
+        "option4": "Steve Jobs",
+        "rightAnswer": "2",
+        "category": "Programieren",
+    },
+    {
+        "question": "who invented the Linux OS?",
+        "option1": "Linus Malte",
+        "option2": "Ken Thompson",
+        "option3": "Brendan Eich",
+        "option4": "Linus Torvalds",
+        "rightAnswer": "4",
+        "category": "Programieren",
+    },
+    {
+        "question": "who are the founders of GooGle?",
+        "option1": "Sundar Pichai",
+        "option2": "Satya Nadella",
+        "option3": "Larry Page and Sergey Brin",
+        "option4": "Anne Wojcicki",
+        "rightAnswer": "3",
+        "category": "Programieren",
+    },
+    {
+        "question": "which Programming language is the easiest to learn at the beginning of a programmer career?",
+        "option1": "Python",
+        "option2": "C++",
+        "option3": "Swift",
+        "option4": "just pick a language and stick to it untill you grasp at least 40% of it :) ",
+        "rightAnswer": "4",
+        "category": "Programieren",
+    },
+    {
+        "question": "which programming language does NASA predominantly use?",
+        "option1": "HAL/S",
+        "option2": "JS Vanilla",
+        "option3": "Python & C#",
+        "option4": "English",
+        "rightAnswer": "1",
+        "category": "Programieren",
+    },
+    {
+        "question": "how do programmers count?",
+        "option1": "1 - 2 - 3 - 4 - 5...",
+        "option2": "duhh.. like a normal human being!",
+        "option3": "0 - 1 - 2 - 3 - 4 - 5...",
+        "option4": "0 - 1 - 0 - 1 - 0 - 1...",
+        "rightAnswer": "3",
+        "category": "Programieren",
+    },
+    {
+        "question": "how do programmers work most of their time?",
+        "option1": "blame others for their bugs!",
+        "option2": "they google and search in Stackoverflow 80% of their time",
+        "option3": "they go outside their house for a walk or some activity",
+        "option4": "they get asked to help 'resetting WiFi Password' or 'hack facbook account'",
+        "rightAnswer": "2",
+        "category": "Programieren",
+    },
+    {
+        "question": "what is the best Method to fix and overcome 'BUGS'?",
+        "option1": "using chemicals like Acid and Sulfur",
+        "option2": "Raid ",
+        "option3": "leaving the House and never coming back!",
+        "option4": "use the Debugger",
+        "rightAnswer": "4",
+        "category": "Programieren",
+    },
+    {
+        "question": "who invented HTML?",
+        "option1": "Lady GaGa",
+        "option2": "Tim Berners-Lee",
+        "option3": "Dr. Frankenstein",
+        "option4": "Steve Jobs",
+        "rightAnswer": "2",
+        "category": "Programieren",
+    },
+    {
+        "question": "who invented HTML?",
+        "option1": "Lady GaGa",
+        "option2": "Tim Berners-Lee",
+        "option3": "Dr. Frankenstein",
+        "option4": "Steve Jobs",
+        "rightAnswer": "2",
+        "category": "Programieren",
     },
 ];
 
@@ -129,20 +229,22 @@ function normalProgress() {
 
 function clickingOnAnswer(answer) {
     let question = questions[currentQuestion];
-    // console.log('Selected answer is', x);
+    console.log('Selected answer is', answer);
     let selectedQuestionEnding = answer.slice(-1);
-    // console.log('selected Question is', selectedQuestionEnding);
-    // console.log('the right Answer is', question['rightAnswer']);
+    console.log('selected Question is', selectedQuestionEnding);
+    console.log('the right Answer is', question['rightAnswer']);
     let idOfRightAnswer = `answer${question['rightAnswer']}`;
     if (selectedQuestionEnding == question['rightAnswer']) {
-        // console.log('Richtige Antwort!');
+        console.log('Richtige Antwort!');
         document.getElementById(answer).parentNode.classList.add('bg-success');
+        document.getElementById('z-index').classList.remove('d-none');
         rightAnswerSound.play();
         rightAnsweredQuestions++;
     } else {
-        // console.log('falsche Antwort!');
+        console.log('falsche Antwort!');
         document.getElementById(answer).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        document.getElementById('z-index').classList.remove('d-none');
         wrongAnswerSound.play();
     }
     document.getElementById('nextButton').disabled = false;
@@ -151,6 +253,7 @@ function clickingOnAnswer(answer) {
 function nextQuestion() {
     currentQuestion++; // beim klicken wird die vaiable um 1 erhöht bzw. kommt die nächste Frage
     document.getElementById('nextButton').disabled = true;
+    document.getElementById('z-index').classList.add('d-none');
     resetAddedClasses();
     askingTheQuestion();
 }
